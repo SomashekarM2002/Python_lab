3cc6ef91eb9fd3a745e048c57b423043756923f5
Somashekar M <somashekarmsoma1@gmail.com>
2025-06-12 20:51:42 +0530

    It is a Fibonacci series

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+def fibonacci_series(n):
+    fib_series = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_series.append(a)
+        a, b = b, a + b
+    return fib_series
+
+# Example usage
+terms= int(input("Enter the number of terms: "))
+if terms <= 0:
+    print("Please enter a positive integer.")
+else:
+    print("Fibonacci series:")
+    print(fibonacci_series(terms))
